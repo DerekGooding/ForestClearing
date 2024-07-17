e379db4e557821546136795a5e25384f76556012
Derek Gooding <djgooding@live.com>
2024-07-17 13:07:37 -0400

    Changed "result" to "response" to match your current standard elsewhere in the project.

--- ForestClearing/MainMethod.cs
+++ ForestClearing/MainMethod.cs
@@ -52,19 +52,19 @@ internal static class MainMethod
         while (string.IsNullOrEmpty(name))
         {
             Write("What is your name: ");
-            string? result = ReadLine();
-            MenuOption? pick = options.Find(x => x.Key.Equals(result, StringComparison.OrdinalIgnoreCase));
+            string? response = ReadLine();
+            MenuOption? pick = options.Find(x => x.Key.Equals(response, StringComparison.OrdinalIgnoreCase));
             if(pick != null)
             {
                 pick.Invoke();
             }
-            else if (string.IsNullOrEmpty(result))
+            else if (string.IsNullOrEmpty(response))
             {
                 WriteLine("I do not understand that.");
             }
             else
             {
-                name = result;
+                name = response;
             }
         }
         return name;
