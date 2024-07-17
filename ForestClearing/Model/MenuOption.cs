fdeddcebc13ed2f141d2217e4f70f7435518a480
Derek Gooding <djgooding@live.com>
2024-07-17 12:12:30 -0400

    Create a generic MenuOption class

---
+++ ForestClearing/Model/MenuOption.cs
@@ -0,0 +1,9 @@
+﻿namespace ForestClearing.Model;
+
+public class MenuOption(string key, Action effect)
+{
+    public string Key { get; } = key;
+    public Action Effect { get; } = effect;
+
+    public void Invoke() => Effect.Invoke();
+}
