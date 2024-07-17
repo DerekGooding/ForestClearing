1c206a16fd6b730d0eddb11169aa66099b5dcceb
Derek Gooding <djgooding@live.com>
2024-07-17 11:34:42 -0400

    Cleaned up redunant code

--- ForestClearing/Program.cs
+++ ForestClearing/Program.cs
@@ -4,37 +4,11 @@ internal static class Program
 {
     static void Main()
     {
-        Console.ForegroundColor = ConsoleColor.Green;
-        //INTRO
+        ForegroundColor = ConsoleColor.Green;
 
         MainMethod.Intro();
-        string? playerName = MainMethod.PlayerName();
+        //string? playerName = MainMethod.PlayerName();
 
         Map.Clearing();
-
-        int x = 0;
-        int y = 0;
-
-        //string input = Console.ReadLine();
-        //if (input == "south")
-        //{
-        //    x -= 1;
-        //    Map.PlayerLocation(x, y);   
-        //}
-        //else if (input == "north")
-        //{
-        //    x += 1;
-        //    Map.PlayerLocation(x, y);
-        //}
-        //else if(input ==  "west")
-        //{
-        //    y -= 1;
-        //    Map.PlayerLocation(x, y);
-        //}
-        //else if (input == "east")
-        //{
-        //    y += 1;
-        //    Map.PlayerLocation(x, y);
-        //}
     }
 }
